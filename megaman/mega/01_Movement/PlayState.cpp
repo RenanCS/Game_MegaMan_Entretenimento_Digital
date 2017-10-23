a6007ddb11a78e3e31a69a984e54399749fbda5d
Matheus Streb <mataca9@gmail.com>
2017-10-23 13:25:30 -0200

    Fixed ogg name

--- megaman/mega/01_Movement/PlayState.cpp
+++ megaman/mega/01_Movement/PlayState.cpp
@@ -136,7 +136,7 @@ void PlayState::init(){
  }
 
 void PlayState::InitSound(){
-    music.openFromFile("data/audio/Stage2.ogg");
+    music.openFromFile("data/audio/Stage.ogg");
     music.setVolume(50);
     music.play();
     music.setLoop(true);
